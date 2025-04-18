75a6ad373adc9be80ced4ee7b1661fcd6fb9cb69
wsleeping <370226263@qq.com>
2025-04-19 00:39:27 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+#Wsleeping Test MCP Server
+
+wsleeping 个人网站测试mcp服务器
+
+## Setup
+
+### NPX
+
+```json
+{
+    "mcpServers": {
+        "wsleeping-mcp-server": {
+            "command": "npx",
+            "args": [
+                "-y",
+                "wsleeping-mcp-server"
+            ],
+            "env": {
+                "WEBSITE_EMAIL": "",
+                "WEBSITE_PASSWORD": ""
+            }
+        }
+    }
+}
+```
---
+++ build/index.js
@@ -0,0 +1,100 @@
+#!/usr/bin/env node
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { CallToolRequestSchema, ListToolsRequestSchema, } from "@modelcontextprotocol/sdk/types.js";
+import fetch from "node-fetch";
+
+// function getParams() {
+//   const email = process.env.WEBSITE_EMAIL;
+//   const password = process.env.WEBSITE_PASSWORD;
+//   if (!email || !password) {
+//       console.error("EMAIL or PASSWORD environment variable is not set");
+//       process.exit(1);
+//   }
+//   return {email, password};
+// }
+// const LOGIN_PARAMS = getParams();
+
+const LOGIN_TOOL = {
+  name: "maps_login",
+  description: "登录高德地图，并返回用户的用户名",
+  inputSchema: {},
+};
+
+
+
+const MAPS_TOOLS = [
+    LOGIN_TOOL
+];
+async function handleLogin() {
+  const url = new URL("http://localhost:3000/api/v1/mcp/login");
+  url.searchParams.append("email", '370226263@qq.com');
+  url.searchParams.append("password", 'qwe123456');
+  const response = await fetch(url.toString());
+  const data = await response.json();
+  if (data.code === 0) {
+    return {
+      content: [{
+        type: "text",
+        text: `欢迎回来, ${data.data.username}`
+      }],
+      isError: false
+    }
+  } else {
+    return {
+      content: [{
+        type: "text",
+        text: `登录失败, ${data.message}`
+      }],
+      isError: true
+    }
+  }
+}
+// Server setup
+const server = new Server({
+    name: "mcp-server/wleeping-mcp-server",
+    version: "0.1.0",
+}, {
+    capabilities: {
+        tools: {},
+    },
+});
+// Set up request handlers
+server.setRequestHandler(ListToolsRequestSchema, async () => ({
+    tools: MAPS_TOOLS,
+}));
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+    try {
+        switch (request.params.name) {
+            case "maps_login": {
+              return await handleLogin();
+            }
+            default:
+                return {
+                    content: [{
+                            type: "text",
+                            text: `Unknown tool: ${request.params.name}`
+                        }],
+                    isError: true
+                };
+        }
+    }
+    catch (error) {
+        return {
+            content: [{
+                    type: "text",
+                    text: `Error: ${error instanceof Error ? error.message : String(error)}`
+                }],
+            isError: true
+        };
+    }
+});
+async function runServer() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("wsleeping MCP Server running on stdio");
+}
+runServer().catch((error) => {
+    console.error("Fatal error running server:", error);
+    process.exit(1);
+});
